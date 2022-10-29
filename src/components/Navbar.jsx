6af29edc8cbaf0f4fc7ea51cd6db2eaead56265f
Shivakant Bajaj <hackindia349@gmail.com>
2022-10-29 16:05:56 +0530

    undo changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,18 @@ import React from "react"
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-sm bg-light">
+    <nav className="navbar">
+      <a className="navbar-brand" href="#">
+        <img src="/logo.png" />
+      </a>
+      <input type="text" className="navbar--search" placeholder="Search for your favorite groups in ATG" />
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <p className="navbar--text">Create account. <a href="#">It's free!</a><i className="ri-arrow-drop-down-line navbar--drop--arrow"></i></p>
+        </li>
+      </ul>
+    </nav>
+    /* <nav className="navbar navbar-expand-sm bg-light">
       <div className="container-fluid d-flex align-items-baseline">
         <a className="navbar-brand" href="#">
           <img src="/logo.png" />
@@ -14,7 +25,7 @@ function Navbar() {
           </li>
         </ul>
       </div>
-    </nav>
+    </nav> */
   )
 }
 
